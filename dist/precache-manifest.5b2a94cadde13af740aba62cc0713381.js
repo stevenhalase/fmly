5d7bdba4ec756a1b7a61f54381a23ad937308d9a
Steven Halase <steven.halase@gmail.com>
2018-10-02 23:19:24 -0600

    fix

---
+++ dist/precache-manifest.5b2a94cadde13af740aba62cc0713381.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "98ecaab97812942f6edd",
+    "url": "/js/dashboard.cc2a306b.js"
+  },
+  {
+    "revision": "4ea01ec2779a8ce6d5f6",
+    "url": "/js/chunk-vendors.16ef76bb.js"
+  },
+  {
+    "revision": "30c89a4114637d960d22",
+    "url": "/js/app.50b484be.js"
+  },
+  {
+    "revision": "d0c0dad2f8086a2bed226f373b7651ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98ecaab97812942f6edd",
+    "url": "/css/dashboard.1119e598.css"
+  },
+  {
+    "revision": "4ea01ec2779a8ce6d5f6",
+    "url": "/css/chunk-vendors.603826f3.css"
+  },
+  {
+    "revision": "30c89a4114637d960d22",
+    "url": "/css/app.936e3797.css"
+  }
+];
--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const port = process.env.PORT || 3090;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 app.listen(port, () => {
   console.log(`Server started at localhost:${port}`);
 })
