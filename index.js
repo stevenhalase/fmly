12acec1cd5bdcdad6dd73eab5150ad9c8c4edc2d
Steven Halase <steven.halase@gmail.com>
2018-10-03 22:41:24 -0500

    Added history package

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const history = require('connect-history-api-fallback');
 const port = process.env.PORT || 3090;
+
+app.use(history());
 
 app.use(express.static(__dirname + '/dist'));
 
